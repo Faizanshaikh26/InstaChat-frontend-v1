5fab9b4ed2cb08d80ce3191d3aa0b691e068a8d6
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-07-18 09:27:32 +0530

    new ui;

--- src/components/shared/ChatItem.jsx
+++ src/components/shared/ChatItem.jsx
@@ -1,3 +1,72 @@
+// import React, { memo } from "react";
+// import { Link } from "../styles/StyledComponents";
+// import { Box, Stack, Typography } from "@mui/material";
+// import AvatarCard from "./AvatarCard";
+// import { motion } from "framer-motion";
+
+// const ChatItem = ({
+//   avatar = [],
+//   name,
+//   _id,
+//   groupChat = false,
+//   sameSender,
+//   isOnline,
+//   newMessageAlert,
+//   index = 0,
+//   handleDeleteChat,
+// }) => {
+//   return (
+//     <Link
+//       sx={{
+//         padding: "0",
+//       }}
+//       to={`/chat/${_id}`}
+//       onContextMenu={(e) => handleDeleteChat(e, _id, groupChat)}
+//     >
+//       <motion.div
+//         initial={{ opacity: 0, y: "-100%" }}
+//         whileInView={{ opacity: 1, y: 0 }}
+//         transition={{ delay: 0.1 * index }}
+//         style={{
+//           display: "flex",
+//           gap: "1rem",
+//           alignItems: "center",
+//           backgroundColor: sameSender ? "black" : "unset",
+//           color: sameSender ? "white" : "unset",
+//           position: "relative",
+//           padding: "1rem",
+//         }}
+//       >
+//         <AvatarCard avatar={avatar} />
+
+//         <Stack>
+//           <Typography>{name}</Typography>
+//           {newMessageAlert && (
+//             <Typography>{newMessageAlert.count} New Message</Typography>
+//           )}
+//         </Stack>
+
+//         {isOnline && (
+//           <Box
+//             sx={{
+//               width: "10px",
+//               height: "10px",
+//               borderRadius: "50%",
+//               backgroundColor: "green",
+//               position: "absolute",
+//               top: "50%",
+//               right: "1rem",
+//               transform: "translateY(-50%)",
+//             }}
+//           />
+//         )}
+//       </motion.div>
+//     </Link>
+//   );
+// };
+
+// export default memo(ChatItem);
+
 import React, { memo } from "react";
 import { Link } from "../styles/StyledComponents";
 import { Box, Stack, Typography } from "@mui/material";
@@ -27,37 +96,20 @@ const ChatItem = ({
         initial={{ opacity: 0, y: "-100%" }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.1 * index }}
-        style={{
-          display: "flex",
-          gap: "1rem",
-          alignItems: "center",
-          backgroundColor: sameSender ? "black" : "unset",
-          color: sameSender ? "white" : "unset",
-          position: "relative",
-          padding: "1rem",
-        }}
+        className={`flex gap-4 items-center p-4 border-b bg-[#ffffff] text-[#111b21] border-white ${sameSender ? 'bg-[#f0f2f5] text-[#0E1514]' : 'text-[#111b21]'} hover:bg-[#f0f2f5] transition duration-200 ease-in-out`}
       >
-        <AvatarCard avatar={avatar} />
-
-        <Stack>
-          <Typography>{name}</Typography>
+         <div className="flex-shrink-0 ">
+        <AvatarCard avatar={avatar}/>
+        </div>
+        <div className="flex flex-col border-b-[1px]  w-full ">
+          <span className="font-bold text-[16px] font-serif mb-2 ml-3">{name}</span>
           {newMessageAlert && (
-            <Typography>{newMessageAlert.count} New Message</Typography>
+            <span className="text-[14px] mb-[2px]" >{newMessageAlert.count} New Message</span>
           )}
-        </Stack>
-
+        </div>
         {isOnline && (
-          <Box
-            sx={{
-              width: "10px",
-              height: "10px",
-              borderRadius: "50%",
-              backgroundColor: "green",
-              position: "absolute",
-              top: "50%",
-              right: "1rem",
-              transform: "translateY(-50%)",
-            }}
+          <div
+            className="w-2.5 h-2.5 rounded-full bg-green-500 absolute top-1/2 right-4 transform -translate-y-1/2"
           />
         )}
       </motion.div>
--- src/components/specific/ChatList.jsx
+++ src/components/specific/ChatList.jsx
@@ -1,3 +1,53 @@
+// import { Stack } from "@mui/material";
+// import React from "react";
+// import ChatItem from "../shared/ChatItem";
+
+// const ChatList = ({
+//   w = "100%",
+//   chats = [],
+//   chatId,
+//   onlineUsers = [],
+//   newMessagesAlert = [
+//     {
+//       chatId: "",
+//       count: 0,
+//     },
+//   ],
+//   handleDeleteChat,
+// }) => {
+//   return (
+//     <Stack width={w} direction={"column"} overflow={"auto"} height={"100%"}>
+//       {chats?.map((data, index) => {
+//         const { avatar, _id, name, groupChat, members } = data;
+
+//         const newMessageAlert = newMessagesAlert.find(
+//           ({ chatId }) => chatId === _id
+//         );
+
+//         const isOnline = members?.some((member) =>
+//           onlineUsers.includes(member)
+//         );
+
+//         return (
+//           <ChatItem
+//             index={index}
+//             newMessageAlert={newMessageAlert}
+//             isOnline={isOnline}
+//             avatar={avatar}
+//             name={name}
+//             _id={_id}
+//             key={_id}
+//             groupChat={groupChat}
+//             sameSender={chatId === _id}
+//             handleDeleteChat={handleDeleteChat}
+//           />
+//         );
+//       })}
+//     </Stack>
+//   );
+// };
+
+// export default ChatList;
 import { Stack } from "@mui/material";
 import React from "react";
 import ChatItem from "../shared/ChatItem";
@@ -16,34 +66,35 @@ const ChatList = ({
   handleDeleteChat,
 }) => {
   return (
-    <Stack width={w} direction={"column"} overflow={"auto"} height={"100%"}>
-      {chats?.map((data, index) => {
-        const { avatar, _id, name, groupChat, members } = data;
+    <div
+    className={`overflow-y-auto h-full box-border  ${w} border-r-[1px] border-[#ABA6A6] shadow-lg`}
+    style={{ backgroundColor: "#FAFAFA", color: "#0E1514" }}
+  >
+    {chats?.map((data, index) => {
+      const { _id, avatar, name, groupChat, members } = data;
 
-        const newMessageAlert = newMessagesAlert.find(
-          ({ chatId }) => chatId === _id
-        );
+      const newMessagesAlerts = newMessagesAlert.find(
+        ({ chatId }) => chatId === _id
+      );
 
-        const isOnline = members?.some((member) =>
-          onlineUsers.includes(member)
-        );
+      const isOnline = members?.some((member) => onlineUsers.includes(member));
 
-        return (
-          <ChatItem
-            index={index}
-            newMessageAlert={newMessageAlert}
-            isOnline={isOnline}
-            avatar={avatar}
-            name={name}
-            _id={_id}
-            key={_id}
-            groupChat={groupChat}
-            sameSender={chatId === _id}
-            handleDeleteChat={handleDeleteChat}
-          />
-        );
-      })}
-    </Stack>
+      return (
+        <ChatItem
+          isOnline={isOnline}
+          name={name}
+          avatar={avatar}
+          groupChat={groupChat}
+          _id={_id}
+          key={_id}
+          samesender={chatId === _id}
+          handleDeleteChat={handleDeleteChat}
+          index={index}
+          newMessageAlert={newMessagesAlerts}
+        />
+      );
+    })}
+  </div>
   );
 };
 
