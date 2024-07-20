9a63428b4b231aadf58163832be38436fdea51d5
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-07-20 15:00:50 +0530

    new2

--- src/components/shared/MessageComponent.jsx
+++ src/components/shared/MessageComponent.jsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React, { memo } from "react";
 import { lightBlue } from "../../constants/color";
 import moment from "moment";
@@ -18,37 +18,21 @@ const MessageComponent = ({ message, user }) => {
       initial={{ opacity: 0, x: "-100%" }}
       whileInView={{ opacity: 1, x: 0 }}
       style={{
-        alignSelf: sameSender ? 'flex-end' : 'flex-start',
-        backgroundColor: sameSender ? '#d9fdd3' : '#ffffff',
-        color: '#111b21',
-        borderRadius: '10px',
-        padding: '1rem',
-        maxWidth: '80%',
-        minWidth: '150px',
-        margin: '0.5rem 0',
-        boxShadow: '0 2px 5px rgba(0,0,0,0.1)',
-        wordWrap: 'break-word',
+        alignSelf: sameSender ? "flex-end" : "flex-start",
+        backgroundColor: "white",
+        color: "black",
+        borderRadius: "5px",
+        padding: "0.5rem",
+        width: "fit-content",
       }}
     >
-
-      <Stack
-      
-      direction='column'
-      spacing={1}
-        sx={{ 
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'wrap',
-          wordBreak: 'break-word',
-        }}
-      >
       {!sameSender && (
-        <Typography color={"#2694ab"} fontWeight={"600"} variant="caption">
+        <Typography color={lightBlue} fontWeight={"600"} variant="caption">
           {sender.name}
         </Typography>
       )}
 
-      {content && <Typography variant="body1"> {content}</Typography>}
+      {content && <Typography>{content}</Typography>}
 
       {attachments.length > 0 &&
         attachments.map((attachment, index) => {
@@ -61,7 +45,9 @@ const MessageComponent = ({ message, user }) => {
                 href={url}
                 target="_blank"
                 download
-                style={{ color: sameSender ? '#004056' : '#2C858D' }}
+                style={{
+                  color: "black",
+                }}
               >
                 {RenderAttachment(file, url)}
               </a>
@@ -72,7 +58,6 @@ const MessageComponent = ({ message, user }) => {
       <Typography variant="caption" color={"text.secondary"}>
         {timeAgo}
       </Typography>
-      </Stack>
     </motion.div>
   );
 };
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,260 +1,65 @@
-import React, {
-  Fragment,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import AppLayout from "../components/layout/AppLayout";
-import { IconButton, Skeleton, Stack } from "@mui/material";
-import { grayColor, orange } from "../constants/color";
-import {
-  AttachFile as AttachFileIcon,
-  Send as SendIcon,
-} from "@mui/icons-material";
-import { InputBox } from "../components/styles/StyledComponents";
-import FileMenu from "../components/dialogs/FileMenu";
-import MessageComponent from "../components/shared/MessageComponent";
-import { getSocket } from "../socket";
-import {
-  ALERT,
-  CHAT_JOINED,
-  CHAT_LEAVED,
-  NEW_MESSAGE,
-  START_TYPING,
-  STOP_TYPING,
-} from "../constants/events";
-import { useChatDetailsQuery, useGetMessagesQuery } from "../redux/api/api";
-import { useErrors, useSocketEvents } from "../hooks/hook";
-import { useInfiniteScrollTop } from "6pp";
-import { useDispatch } from "react-redux";
-import { setIsFileMenu } from "../redux/reducers/misc";
-import { removeNewMessagesAlert } from "../redux/reducers/chat";
-import { TypingLoader } from "../components/layout/Loaders";
-import { useNavigate } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import React, { memo } from "react";
+import { lightBlue } from "../../constants/color";
+import moment from "moment";
+import { fileFormat } from "../../lib/features";
+import RenderAttachment from "./RenderAttachment";
+import { motion } from "framer-motion";
 
-const Chat = ({ chatId, user }) => {
-  const socket = getSocket();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+const MessageComponent = ({ message, user }) => {
+  const { sender, content, attachments = [], createdAt } = message;
 
-  const containerRef = useRef(null);
-  const bottomRef = useRef(null);
+  const sameSender = sender?._id === user?._id;
 
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [fileMenuAnchor, setFileMenuAnchor] = useState(null);
+  const timeAgo = moment(createdAt).fromNow();
 
-  const [IamTyping, setIamTyping] = useState(false);
-  const [userTyping, setUserTyping] = useState(false);
-  const typingTimeout = useRef(null);
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: "-100%" }}
+      whileInView={{ opacity: 1, x: 0 }}
+      style={{
+        alignSelf: sameSender ? "flex-end" : "flex-start",
+        backgroundColor: "white",
+        color: "black",
+        borderRadius: "5px",
+        padding: "0.5rem",
+        width: "fit-content",
+      }}
+    >
+      {!sameSender && (
+        <Typography color={lightBlue} fontWeight={"600"} variant="caption">
+          {sender.name}
+        </Typography>
+      )}
 
-  const chatDetails = useChatDetailsQuery({ chatId, skip: !chatId });
+      {content && <Typography>{content}</Typography>}
 
-  const oldMessagesChunk = useGetMessagesQuery({ chatId, page });
+      {attachments.length > 0 &&
+        attachments.map((attachment, index) => {
+          const url = attachment.url;
+          const file = fileFormat(url);
 
-  const { data: oldMessages, setData: setOldMessages } = useInfiniteScrollTop(
-    containerRef,
-    oldMessagesChunk.data?.totalPages,
-    page,
-    setPage,
-    oldMessagesChunk.data?.messages
-  );
+          return (
+            <Box key={index}>
+              <a
+                href={url}
+                target="_blank"
+                download
+                style={{
+                  color: "black",
+                }}
+              >
+                {RenderAttachment(file, url)}
+              </a>
+            </Box>
+          );
+        })}
 
-  const errors = [
-    { isError: chatDetails.isError, error: chatDetails.error },
-    { isError: oldMessagesChunk.isError, error: oldMessagesChunk.error },
-  ];
-
-  const members = chatDetails?.data?.chat?.members;
-
-  const messageOnChange = (e) => {
-    setMessage(e.target.value);
-
-    if (!IamTyping) {
-      socket.emit(START_TYPING, { members, chatId });
-      setIamTyping(true);
-    }
-
-    if (typingTimeout.current) clearTimeout(typingTimeout.current);
-
-    typingTimeout.current = setTimeout(() => {
-      socket.emit(STOP_TYPING, { members, chatId });
-      setIamTyping(false);
-    }, [2000]);
-  };
-
-  const handleFileOpen = (e) => {
-    dispatch(setIsFileMenu(true));
-    setFileMenuAnchor(e.currentTarget);
-  };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-
-    if (!message.trim()) return;
-
-    // Emitting the message to the server
-    socket.emit(NEW_MESSAGE, { chatId, members, message });
-    setMessage("");
-  };
-
-  useEffect(() => {
-    socket.emit(CHAT_JOINED, { userId: user._id, members });
-    dispatch(removeNewMessagesAlert(chatId));
-
-    return () => {
-      setMessages([]);
-      setMessage("");
-      setOldMessages([]);
-      setPage(1);
-      socket.emit(CHAT_LEAVED, { userId: user._id, members });
-    };
-  }, [chatId]);
-
-  useEffect(() => {
-    if (bottomRef.current)
-      bottomRef.current.scrollIntoView({ behavior: "smooth" });
-  }, [messages]);
-
-  useEffect(() => {
-    if (chatDetails.isError) return navigate("/");
-  }, [chatDetails.isError]);
-
-  const newMessagesListener = useCallback(
-    (data) => {
-      if (data.chatId !== chatId) return;
-
-      setMessages((prev) => [...prev, data.message]);
-    },
-    [chatId]
-  );
-
-  const startTypingListener = useCallback(
-    (data) => {
-      if (data.chatId !== chatId) return;
-
-      setUserTyping(true);
-    },
-    [chatId]
-  );
-
-  const stopTypingListener = useCallback(
-    (data) => {
-      if (data.chatId !== chatId) return;
-      setUserTyping(false);
-    },
-    [chatId]
-  );
-
-  const alertListener = useCallback(
-    (data) => {
-      if (data.chatId !== chatId) return;
-      const messageForAlert = {
-        content: data.message,
-        sender: {
-          _id: "djasdhajksdhasdsadasdas",
-          name: "Admin",
-        },
-        chat: chatId,
-        createdAt: new Date().toISOString(),
-      };
-
-      setMessages((prev) => [...prev, messageForAlert]);
-    },
-    [chatId]
-  );
-
-  const eventHandler = {
-    [ALERT]: alertListener,
-    [NEW_MESSAGE]: newMessagesListener,
-    [START_TYPING]: startTypingListener,
-    [STOP_TYPING]: stopTypingListener,
-  };
-
-  useSocketEvents(socket, eventHandler);
-
-  useErrors(errors);
-
-  const allMessages = [...oldMessages, ...messages];
-
-  return chatDetails.isLoading ? (
-    <Skeleton />
-  ) : (
-    <Fragment>
-      <Stack
-        ref={containerRef}
-        boxSizing={"border-box"}
-        padding={"1rem"}
-        spacing={"1rem"}
-        bgcolor={grayColor}
-        height={"90%"}
-        sx={{
-          overflowX: "hidden",
-          overflowY: "auto",
-        }}
-      >
-        {allMessages.map((i) => (
-          <MessageComponent key={i._id} message={i} user={user} />
-        ))}
-
-        {userTyping && <TypingLoader />}
-
-        <div ref={bottomRef} />
-      </Stack>
-
-      <form
-        style={{
-          height: "10%",
-        }}
-        onSubmit={submitHandler}
-      >
-        <Stack
-          direction={"row"}
-          height={"100%"}
-          padding={"1rem"}
-          alignItems={"center"}
-          position={"relative"}
-        >
-          <IconButton
-            sx={{
-              position: "absolute",
-              left: "1.5rem",
-              rotate: "30deg",
-            }}
-            onClick={handleFileOpen}
-          >
-            <AttachFileIcon />
-          </IconButton>
-
-          <InputBox
-            placeholder="Type Message Here..."
-            value={message}
-            onChange={messageOnChange}
-          />
-
-          <IconButton
-            type="submit"
-            sx={{
-              rotate: "-30deg",
-              bgcolor: orange,
-              color: "white",
-              marginLeft: "1rem",
-              padding: "0.5rem",
-              "&:hover": {
-                bgcolor: "error.dark",
-              },
-            }}
-          >
-            <SendIcon />
-          </IconButton>
-        </Stack>
-      </form>
-
-      <FileMenu anchorE1={fileMenuAnchor} chatId={chatId} />
-    </Fragment>
+      <Typography variant="caption" color={"text.secondary"}>
+        {timeAgo}
+      </Typography>
+    </motion.div>
   );
 };
 
-export default AppLayout()(Chat);
+export default memo(MessageComponent);
